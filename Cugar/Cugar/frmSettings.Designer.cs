26dfc5d14d4f90a89183d1202868b4238219bc2e
Mathias Mader <skull@darktemple.ch>
2011-09-22 15:40:19 +0200

    settings dialog added

---
+++ Cugar/Cugar/frmSettings.Designer.cs
@@ -0,0 +1,46 @@
+﻿namespace Cugar
+{
+    partial class frmSettings
+    {
+        /// <summary>
+        /// Erforderliche Designervariable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Verwendete Ressourcen bereinigen.
+        /// </summary>
+        /// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Vom Windows Form-Designer generierter Code
+
+        /// <summary>
+        /// Erforderliche Methode für die Designerunterstützung.
+        /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // frmSettings
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(383, 142);
+            this.Name = "frmSettings";
+            this.Text = "frmSettings";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
